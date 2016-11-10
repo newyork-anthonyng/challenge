1f346845f24663ad537768b210d9e1b270366cfb
Anthony Ng <newyork.anthonyng@gmail.com>
2016-11-10 13:18:07 -0500

    Update url to use https

--- src/Logic.js
+++ src/Logic.js
@@ -1,7 +1,7 @@
 import fetchJsonp from 'fetch-jsonp';
 
 const ACCOUNT_ID = '883610';
-const URL = `http://api.bigstockphoto.com/2/${ACCOUNT_ID}/search/?q=`;
+const URL = `https://api.bigstockphoto.com/2/${ACCOUNT_ID}/search/?q=`;
 
 const getImages = (searchTerm) => {
 	return fetchJsonp(URL + searchTerm)
